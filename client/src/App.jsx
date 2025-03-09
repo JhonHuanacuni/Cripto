344ab917805ba5c7c3c6b558cc6b8a81bb5c8528
JhonHuanacuni <jhon.huanacuni.mamani@gmail.com>
2025-03-09 16:14:19 -0500

    Django y React conectados

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,21 @@ import { TasksPage } from "./pages/TasksPage";
 import { TaskFormPage } from "./pages/TaskFormPage";
 import { Navigation } from "./components/Navigation";
 import { Toaster } from "react-hot-toast";
+
+
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Navigate to="/tasks" />} />
-        <Route path="/tasks" element={<TasksPage />} />
-        <Route path="/tasks-create" element={<TaskFormPage />} />
-        <Route path="/tasks/:id" element={<TaskFormPage />} />
-      </Routes>
-      <Toaster/> 
+      <div className="container mx-auto">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Navigate to="/tasks" />} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/tasks-create" element={<TaskFormPage />} />
+          <Route path="/tasks/:id" element={<TaskFormPage />} />
+        </Routes>
+        <Toaster/>
+      </div>
     </BrowserRouter>
   );
 }
--- client/src/components/TasksList.jsx
+++ client/src/components/TasksList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getAllTasks } from "../api/tasks.api"; 
+import { getAllTasks } from "../api/tasks.api";
 import { TaskCard } from "./TaskCard";
 
 export function TasksList() {
@@ -9,16 +9,16 @@ export function TasksList() {
     async function loadTasks() {
       const res = await getAllTasks();
       setTasks(res.data);
-      console.log(res);
     }
     loadTasks();
   }, []);
 
   return (
-    <div>
-      {tasks.map((task) => ( //recorre todas la tareas y muestra 
+    <div className="grid grid-cols-3 gap-3">
+      {tasks.map((task) => (  //recorre todas la tareas y muestra
         <TaskCard key={task.id} task={task} />
       ))}
     </div>
   );
 }
+
